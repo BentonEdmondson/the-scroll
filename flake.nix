3e585c3eb78ce53f7b34918cdd3e7ef3266f3494
benton <bentonedmondson@gmail.com>
2021-07-25 18:37:25 -0500

    switched from url syntax to strings

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, ... }@flakes: let
     nixpkgs = flakes.nixpkgs.legacyPackages.x86_64-linux;
