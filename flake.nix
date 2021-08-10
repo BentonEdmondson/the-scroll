81a64c7210090b2f6364a37193a9f44c43c8fe55
benton <bentonedmondson@gmail.com>
2021-08-10 12:37:39 -0500

    fixed up flake.nix

--- flake.nix
+++ flake.nix
@@ -5,30 +5,34 @@
     nixpkgs = flakes.nixpkgs.legacyPackages.x86_64-linux;
   in {
     defaultPackage.x86_64-linux = nixpkgs.stdenv.mkDerivation {
-        pname = "libgourou-utils";
-        version = "0.3.1";
-        src = self;
-        nativeBuildInputs = [ nixpkgs.pkg-config ];
-        buildInputs = [ nixpkgs.openssl nixpkgs.qt5.qtbase nixpkgs.libzip nixpkgs.pugixml ];
-        installPhase = ''
-            mkdir -p $out/bin $out/lib
-            cp libgourou.so $out/lib
-            cp utils/adept_activate $out/bin/adept-register
-            cp utils/acsmdownloader $out/bin/adept-download
-        '';
-        dontWrapQtApps = true;
+      pname = "libgourou-utils";
+      version = "0.3.1";
+      src = self;
 
-        meta = {
-          description = "An implementation of Adobe ADEPT for Linux";
-          homepage = "https://github.com/BentonEdmondson/libgourou-utils";
-          license = [ nixpkgs.lib.licenses.lgpl3Plus nixpkgs.lib.licenses.bsd3 ];
-          maintainers = [{
-            name = "Benton Edmondson";
-            email = "bentonedmondson@gmail.com";
-          }];
-          # potentially others, but I'm only listed those tested
-          platforms = [ "x86_64-linux" ];
-        };
+      nativeBuildInputs = [ nixpkgs.pkg-config ];
+
+      buildInputs = [ nixpkgs.openssl nixpkgs.qt5.qtbase nixpkgs.libzip nixpkgs.pugixml ];
+  
+      installPhase = ''
+          mkdir -p $out/bin $out/lib
+          cp libgourou.so $out/lib
+          cp utils/adept_activate $out/bin/adept-register
+          cp utils/acsmdownloader $out/bin/adept-download
+      '';
+
+      dontWrapQtApps = true;
+
+      meta = {
+        description = "An implementation of Adobe ADEPT for Linux";
+        homepage = "https://github.com/BentonEdmondson/libgourou-utils";
+        license = [ nixpkgs.lib.licenses.lgpl3Plus nixpkgs.lib.licenses.bsd3 ];
+        maintainers = [{
+          name = "Benton Edmondson";
+          email = "bentonedmondson@gmail.com";
+        }];
+        # potentially others, but I'm only listed those tested
+        platforms = [ "x86_64-linux" ];
       };
+    };
   };
 }
